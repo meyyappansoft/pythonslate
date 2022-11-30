b112a468a75ae786c87ad4ba2b7cd3cadf7d321a
meyyappansoft <119500733+meyyappansoft@users.noreply.github.com>
2022-11-30 18:37:26 +0530

    quiz

--- src/main/com/projects/game/Quiz.py
+++ src/main/com/projects/game/Quiz.py
@@ -1,1 +1,15 @@
-print("simple quiz game")
+print("Welcome to quiz!")
+playerName = input("Enter your name : ")
+if playerName == "":
+    print("You have not entered your name!")
+    quit()
+print(playerName +  ", Lets start.")
+
+score=0
+
+q1 = input("What does CPU stand for? ").lower()
+if q1 == "central processing unit":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect! better luck next time..")
